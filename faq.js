de9e4be9f95561e75bbafd26daadec5877290000
Jackson Chen <jackson.chen@columbia.edu>
2017-08-06 13:16:49 -0400

    Add FAQ triangle indicators

--- faq.js
+++ faq.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
   $('.question').click(function() {
-    $(this).next().stop(true, true).slideToggle();
+    var elem = $(this).next();
+    if (elem.css('display') == 'none') {
+      $(this).find('span').removeClass('glyphicon-triangle-bottom');
+      $(this).find('span').addClass('glyphicon-triangle-top');
+    }
+    else {
+      $(this).find('span').removeClass('glyphicon-triangle-top');
+      $(this).find('span').addClass('glyphicon-triangle-bottom');
+    }
+
+    elem.stop(true, true).slideToggle();
   });
 });
