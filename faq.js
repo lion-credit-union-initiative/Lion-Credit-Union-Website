93ecf3cd0a6dd5b70307c64c05577e67d36b6fbe
Kevin L Wei <kevin.wei@columbia.edu>
2017-08-23 04:35:35 -0400

    adding font-awesome to replace glyphicons

--- faq.js
+++ faq.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   $('.question').click(function() {
-    var elem = $(this).next();
+    var elem = $(this).next(".answer");
     if (elem.css('display') == 'none') {
-      $(this).find('span').removeClass('glyphicon-triangle-bottom');
-      $(this).find('span').addClass('glyphicon-triangle-top');
+      $(this).find('span').removeClass('fa-caret-down');
+      $(this).find('span').addClass('fa-caret-up');
     }
     else {
-      $(this).find('span').removeClass('glyphicon-triangle-top');
-      $(this).find('span').addClass('glyphicon-triangle-bottom');
+      $(this).find('span').removeClass('fa-caret-up');
+      $(this).find('span').addClass('fa-caret-down');
     }
 
     elem.stop(true, true).slideToggle();
